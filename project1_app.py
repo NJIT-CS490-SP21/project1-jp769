137b044a579cb473813ef7d4d3415c0254771883
EC2 Default User <ec2-user@ip-172-31-13-184.ec2.internal>
2021-02-05 17:20:04 +0000

    implemented random selection for artists and song, successfully parsing request to get artist name and song

--- project1_app.py
+++ project1_app.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv, find_dotenv
+import random
 
 # URL to send request for app authorization (get access token)
 AUTH_URL = 'https://accounts.spotify.com/api/token'
@@ -23,8 +24,38 @@ response_data = response.json()
 # parsing/accessing only the access_token from response
 access_token = response_data['access_token']
 
+# print(access_token)
+
 #header info with access_token ^ used in GET request to the API server
 headers = {
     'Authorization': 'Bearer {token}'.format(token=access_token)
 }
 
+favorite_artists_id = [
+    '0fA0VVWsXO9YnASrzqfmYu',
+    '4MCBfE4596Uoi2O4DtmEMz',
+    '5INjqkS1o8h1imAzPqGZBb',
+    '4q3ewBCX7sLwd24euuV69X',
+    '2h93pZq0e7k5yf4dywlkpM',
+    '4O15NlyKLIASxsJ0PrXPfz',
+    '1RyvyyTE3xzB2ZywiAwp0i',
+    '1tq9rmv85VHcxqvNdO1DQP'
+]
+
+random_number = random.randint(0, len(favorite_artists_id)-1)
+# print(len(favorite_artists_id), random_number)
+print(favorite_artists_id[random_number])
+
+market='?market=US'
+
+# Fetching top tracks of artist
+r = requests.get(BASE_URL + 'artists/{id}/top-tracks'.format(id=favorite_artists_id[random_number]) + market, headers=headers)
+
+r = r.json()
+
+random_number = random.randint(0, len(r['tracks']))
+
+print(len(r['tracks']), random_number)
+
+print(r['tracks'][random_number]['album']['artists'][0]['name'])
+print(r['tracks'][random_number]['name'])
