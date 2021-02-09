2e19df8a867d34dda880f899b6aa8d9d0778d83f
EC2 Default User <ec2-user@ip-172-31-15-130.ec2.internal>
2021-02-09 00:52:18 +0000

    applied style to display other tracks in grid, changed how variables from get_data are passed onto template

--- project1_app.py
+++ project1_app.py
@@ -13,23 +13,23 @@ def hello_world():
     print('\nRan, updated.\n')
     
     spotify_data = get_data()
-    print(spotify_data['main_song']['song_name'])
-    print(spotify_data['main_song']['song_image'])
-    print(spotify_data['other_songs']['song_names'])
-    print(spotify_data['other_songs']['song_image'])
+    # print(spotify_data['main_song']['song_name'])
+    # print(spotify_data['main_song']['song_image'])
+    # print(spotify_data['other_songs']['song_names'])
+    # print(spotify_data['other_songs']['song_image'])
     # print(spotify_data['json_r'])
 
     return render_template(
         "index.html",
         song_image = spotify_data['main_song']['song_image'],
         song_name = spotify_data['main_song']['song_name'],
-        other_songs = spotify_data['other_songs']['song_names'],
-        other_songs = spotify_data['other_songs']['song_image'],
-        num_songs = len(spotify_data['other_songs']['song_names'])
-    #     artist = spotify_data['main_artist'],
-    #     num_artists = spotify_data['num_artists'],
-    #     all_artists = spotify_data['all_artists'],
-    #     song_preview = spotify_data['song_preview'],
+        artist = spotify_data['main_song']['main_artist'],
+        num_artists = spotify_data['main_song']['num_artists'],
+        all_artists = spotify_data['main_song']['artists'],
+        song_preview = spotify_data['main_song']['song_preview'],
+        other_songs_names = spotify_data['other_songs']['song_names'],
+        other_songs_images = spotify_data['other_songs']['song_image'],
+        num_songs = len(spotify_data['other_songs']['song_names']),
     )
 
 app.run(
