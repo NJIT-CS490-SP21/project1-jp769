64ba6c7fb126718a8acb91d1c254875ae954b7e6
EC2 Default User <ec2-user@ip-172-31-15-130.ec2.internal>
2021-02-09 04:06:49 +0000

    cleaning up code and adding basic comments to describe code

--- spotify_api.py
+++ spotify_api.py
@@ -37,29 +37,28 @@ def get_data():
     # parsing/accessing only the access_token from response
     access_token = response_data['access_token']
     
-    # print(access_token)
+    # print(access_token) #TESTING PURPOSES ONLY!! Needed if trying spotify api on web console
     
     #header info with access_token ^ used in GET request to the API server
     headers = {
         'Authorization': 'Bearer {token}'.format(token=access_token)
     }
     
+    # Randomly choosing an artist from list
     random_artist = random.randint(0, len(favorite_artists_id)-1)
-    # print(len(favorite_artists_id), random_number) #debugging purpose only
-    # print(favorite_artists_id[random_number]) # debugigng purpose only
     
     market='?market=US'
     
     # Fetching top tracks of artist
     r = requests.get(BASE_URL + 'artists/{id}/top-tracks'.format(id=favorite_artists_id[random_artist]) + market, headers=headers)
     
+    # 
     r = r.json()
     
+    # Randomly choosing a song from the given top tracks
     random_song = random.randint(0, len(r['tracks'])-1)
     
-    # print(len(r['tracks']), random_number) #debugging purpose only
-    
-    # json_r = r['tracks'][random_song]['artists']
+    # Information of main song (randomly choosen) stored in dictionary
     main = {
         'song_name': r['tracks'][random_song]['name'],
         'song_image': r['tracks'][random_song]['album']['images'][1]['url'],
@@ -69,30 +68,25 @@ def get_data():
         'artists': []
     }
     
+    # Dictionary holding the other top tracks of artist (list of the names and images)
     other_tracks={
         'song_names': [],
         'song_image': []
     }
+    
+    # Population artist(s) and main artist data in main dictionary
     for i in range (0,main['num_artists']):
         if(r['tracks'][random_song]['artists'][i]['id'] == favorite_artists_id[random_artist]):
             main['main_artist'] = r['tracks'][random_song]['artists'][i]['name']
         main['artists'].append(r['tracks'][random_song]['artists'][i]['name'])
-        
-    # print(artists)
+    
+    #Population other tracks with name and image to display
     for i in range (0,len(r['tracks'])):
         if(i != random_song):
             other_tracks['song_names'].append(r['tracks'][i]['name'])
             other_tracks['song_image'].append(r['tracks'][i]['album']['images'][2]['url'])
 
     return {
-        # 'r': r,
-        # 'json_r': json_r,
-        # 'song_image': song_image,
-        # 'num_artists': num_artists,
-        # 'main_artist': main_artist,
-        # 'song_name': song_name,
-        # 'all_artists': artists,
-        # 'song_preview': song_preview,
         'main_song': main,
         'other_songs': other_tracks
     }
